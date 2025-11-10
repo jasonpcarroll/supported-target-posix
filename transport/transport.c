58922c2b476d08e068d1c9eef07d17d1e796990c
czjaso <czjaso@amazon.com>
2025-11-10 10:01:15 -0800

    Add fcntl.h include for socket flags debugging

--- transport/transport.c
+++ transport/transport.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <poll.h>
+#include <fcntl.h>
 #include <openssl/err.h>
 
 static TransportStatus_t setupTlsConnection( NetworkContext_t * pNetworkContext,
